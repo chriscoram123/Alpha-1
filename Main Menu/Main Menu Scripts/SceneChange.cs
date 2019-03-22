5021f6e92f135601cd4a14d15b1d5299309688d8
Christopher Coram <ultraartist15@gmail.com>
2019-03-22 17:18:02 -0400

    Add files via upload

---
+++ Main Menu/Main Menu Scripts/SceneChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour {
+    // Scene changes with onclick with object
+   public void OnMouseDown() {
+        SceneManager.LoadScene("Level 1", LoadSceneMode.Additive);
+        Update();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // Notifies when code is working
+        Debug.Log("CHANGE SCENE");
+    }
+}
